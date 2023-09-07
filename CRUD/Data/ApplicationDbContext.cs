f03107b4c747a4c9651b42436a530a895245b69f
Bibek Parajuli <parajulibbek11@gmail.com>
2023-09-07 19:14:41 +0545

    initial commit

---
+++ CRUD/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace CRUD.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
